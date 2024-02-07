78aa7d2478b5c2626c708e6d5dea6e740fa8cf23
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2024-02-07 18:39:45 +0100

    txtreader update

--- txtreader.py
+++ txtreader.py
@@ -7,9 +7,26 @@ def read_txt(filename):
     with open(filename, 'r') as file:
         raw_txt = file.read()
 
+    txt = getDfTxt(raw_txt)
+    
     mymeta = getMetaTxt(filename)
     
-    return mymeta, raw_txt
+    return mymeta, txt
+
+def getDfTxt(raw_txt):
+
+    # Splitting raw_txt into lines and removing any leading/trailing whitespaces
+    lines = [line.strip() for line in raw_txt.split('\n')]
+
+    # Extracting column names from the first line (assuming it contains headers)
+    columns = [column.strip() for column in lines[0].split(',')]
+
+    # Extracting data from subsequent lines
+    data = [line.split(',') for line in lines[1:]]
+
+    # Make DataFrame
+    df = pd.DataFrame(data, columns=columns)
+    return df
 
 
 def getMetaTxt(filename):
